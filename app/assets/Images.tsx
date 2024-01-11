f37397cb9201689eb47ce185186249bb25126cd8
Tom-Mai <mailleux.tom@gmail.com>
2024-01-11 12:36:02 +0100

    creat app file , add img, logo

---
+++ app/assets/Images.tsx
@@ -0,0 +1,77 @@
+import hyginealimentaire from "./hyginealimentaire.webp";
+import lvlreactcorp from "./lvlreactcorp.webp";
+import repres1 from './repres1.webp';
+import repres2 from './repres2.webp';
+import repres3 from './repres3.webp';
+import repres4 from './repres4.webp';
+import repres5 from './repres5.webp';
+import repres6 from './repres6.webp';
+import repres7 from './repres7.webp';
+import repres8 from './repres8.webp';
+import repres9 from './repres9.webp';
+import repres10 from './repres10.webp';
+import MaurineDubeuf from "./MaurineDubeuf.jpg";
+
+export const Images = {
+    Maurinedb :[
+        {
+            src: MaurineDubeuf,
+            alt: 'Photo portrait de Maurine Dubeuf, Naturopathe'
+        }
+    ],
+    hyginealimentaire: [
+      {
+        src:    hyginealimentaire,
+        alt: 'Représentation Hygiene alimentaire',
+    },
+],
+lvlreactcorp:[
+    {
+        src:lvlreactcorp,
+        alt:'Représentation des niveaux de reaction corporel'
+    }
+],
+    repres:[
+        {
+        src:repres1,
+        alt:'systeme, nerveux',
+    },
+    {
+        src:repres2,
+        alt:'asthme, sinusite, essouflement ',
+    },
+    {
+        src:repres3,
+        alt:',cystite lithiase',
+    },
+    {
+        src:repres4,
+        alt:'arthrose, osteoporose, polyarthrite, rhumatoïde',
+    },
+    {
+        src:repres5,
+        alt:'diarrhée / constipation, ballonnement, remontée acide, maladie de Crohn',
+    },
+    {
+        src:repres6,
+        alt:`baisse d'énergie, baisse de l'immunité`,
+    },
+    {
+        src:repres7,
+        alt:'eczéma,psoriasis,acné',
+    },
+    {
+        src:repres8,
+        alt:'hypothyroïdie, syndrome de Cushing, diabète',
+    },
+    {
+        src:repres9,
+        alt:'varice',
+    },
+    {
+        src:repres10,
+        alt:'endométriose, SOPK, syndrome pré-menstuel, adénome de la prostate',
+    },
+    ]};
+   
+
---
+++ app/assets/MyHerosSectionImage.tsx
@@ -0,0 +1,2 @@
+export const MyHerosSectionImage = "https://primary.jwwb.nl/unsplash/kXQ3J7_2fpc.jpg?enable-io=true&fit=bounds&width=1920&height=1920";
+
