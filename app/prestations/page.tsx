7805b59e094c8accdfdb5fd19c2853a7d9fcf287
Tom-Mai <mailleux.tom@gmail.com>
2024-01-15 16:55:44 +0100

    add first component

---
+++ app/prestations/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+
+// import Sanity from "../../Sanity";
+import { Images } from "@/app/assets/Images";
+import { HeroSection } from "@/src/features/layout/HeroSection";
+import { SectionContent } from "@/src/features/layout/SectionContent";
+
+export default function prestations() {
+  return (
+    <Grid>
+    <HeroSection/>
+    <SectionContent/>
+    </Grid>
+  )
+}
