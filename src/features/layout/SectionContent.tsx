0d681825ecc3a56efdeb3ec6f2a4ca8ea3d13550
Tom-Mai <mailleux.tom@gmail.com>
2024-01-19 17:53:00 +0100

    add funct for highltight element

--- src/features/layout/SectionContent.tsx
+++ src/features/layout/SectionContent.tsx
@@ -6,40 +6,59 @@ import Grid from "@mui/material/Grid";
 import { Titreh2 } from "./Titreh2";
 import Image, { StaticImageData } from "next/image";
 
-interface SectionContentProps {
-  imgSrc?:{
-    src: StaticImageData;
-    alt: string;
-  };
-  bodyText?: string;
-  titreText?: string;
-  imgToLeft?: boolean;
+//  fonction qui prend mon tableau de mots cles, et si le mot cles se trouve dans la props bodytext, celui ci prends une autre classe de style afin d'être mis en évidence
+function highlightText(text: string, keywords: string[]): React.ReactNode {
+    const regex = new RegExp(`(${keywords.join('|')})`, 'gi');
+    return text.split(regex).map((part, index) => {
+        if (keywords.includes(part.toLowerCase())) {
+            return (
+                <strong key={index} className="highlight">{part}</strong>
+            )
+        } else {
+            return part;
+        }
+    });
 }
-export const SectionContent = ({ imgSrc, bodyText, titreText, imgToLeft }:SectionContentProps ) => {
+
+interface SectionContentProps {
+    imgSrc?: {
+        src: StaticImageData;
+        alt: string;
+    };
+    bodyText?: React.ReactNode;
+    titreText?: string;
+    imgToLeft?: boolean;
+}
+export const SectionContent = ({ imgSrc, bodyText, titreText, imgToLeft }: SectionContentProps) => {
+
+    const highlightWords = ["naturopathie", "Organisation Mondiale de la Santé", "méthodes de soins", "moyens naturels et biologiques", "approche holistique", "individu", "naturopathe", "symptômes", "hygiène de vie", "accompagnement individualisé", "auto-guérison", "médecine traditionnelle chinoise", "ayurvédique", "diagnostic", "bien-être", "hygiène alimentaire", "musculaire", "neuropsychique", "hydrologie", "phytologie", "aromatologie", "réflexologie plantaire", "exercices respiratoires", "massages", "magnétisme", "fatigue", "énergie", "problèmes digestifs", "dysfonctionnement", "symptôme", "pluridisciplinaire", "déséquilibre", "réactions corporelles", "suivi médical", "prévention", "maladie"];
+
+    const formattedBodyText = typeof bodyText === 'string' ? highlightText(bodyText, highlightWords) : bodyText;
     const textComponent = (
         <Grid item xs={12} lg={imgSrc ? 6 : 12}>
-            {bodyText &&
+            {formattedBodyText &&
                 <Typography variant='body1' sx={{
                     marginTop: '20px',
                     lineHeight: "1.35",
                     fontSize: "1.65rem",
-                    color:'#707070',
+                    color: '#707070',
                     textAlign: "justify",
                 }}>
-                    {bodyText}
+                    {formattedBodyText}
                 </Typography>}
         </Grid>
     );
-  
-    const imgComponent = imgSrc && 
-        <Grid item  sx={{
-            maxWidth:'400px',
-            margin:'auto',
-            "@media (max-width:1200px)":{
-            width: "100%",
-            justifyContent: "center",            
-        },}}>
-       <Image src={imgSrc.src} alt={imgSrc.alt} layout='responsive' />
+
+    const imgComponent = imgSrc &&
+        <Grid item sx={{
+            maxWidth: '400px',
+            margin: 'auto',
+            "@media (max-width:1200px)": {
+                width: "100%",
+                justifyContent: "center",
+            },
+        }}>
+            <Image src={imgSrc.src} alt={imgSrc.alt} layout='responsive' />
 
         </Grid>;
 
@@ -54,7 +73,7 @@ export const SectionContent = ({ imgSrc, bodyText, titreText, imgToLeft }:Sectio
             }}
         >
             {titreText && <Titreh2 textContent={titreText} />}
-            <Grid container spacing={2} sx={{justifyContent:'space-evenly'}}>
+            <Grid container spacing={2} sx={{ justifyContent: 'space-evenly' }}>
                 {imgToLeft ? imgComponent : textComponent}
                 {imgToLeft ? textComponent : imgComponent}
             </Grid>
