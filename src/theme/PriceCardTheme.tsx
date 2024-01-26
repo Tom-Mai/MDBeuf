9ae0a429ba84136bb6f270b6acbc1e565bb252b3
Tom-Mai <mailleux.tom@gmail.com>
2024-01-26 18:21:08 +0100

    change height card

--- src/theme/PriceCardTheme.tsx
+++ src/theme/PriceCardTheme.tsx
@@ -4,7 +4,7 @@ import { styled } from '@mui/material/styles';
 import { Card, Paper } from "@mui/material";
 export const PriceCardTheme = styled(Card)(({ theme })=> ({
     width:300,
-    height:600,
+    height:"fitContent",
     padding: theme.spacing(2),
     ...theme.typography.body2,
     textAlign: 'center',
