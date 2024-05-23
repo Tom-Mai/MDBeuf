c9123166f2bd5c49a711e9d5bbb9dada5dcc6533
Tom-Mai <mailleux.tom@gmail.com>
2024-05-23 17:01:55 +0200

    change size button

--- src/features/layout/ScrollToTop.tsx
+++ src/features/layout/ScrollToTop.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import {IconButton } from "@mui/material";
 import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+import { ChevronsUp } from "lucide-react";
 export default function ScrollTopTop() {
     const [showButton, setShowButton] = useState(false);
 
@@ -27,11 +28,12 @@ export default function ScrollTopTop() {
             style={{
                 display: showButton ? "block" : "none",
                 position: "fixed",
-                bottom: "10px",
-                right: "10px"
+                bottom: "5px",
+                right: "0px"
             }}
         >
-            <ArrowCircleUpIcon fontSize="large"/>
+            <ChevronsUp size={40} strokeWidth={1} color="#b08d57"/>
+            
             </IconButton> 
         
     );
