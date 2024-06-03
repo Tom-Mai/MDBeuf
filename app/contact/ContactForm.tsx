1230b3bcb9a5d015bf3c16f09d57a041b5921f96
Tom-Mai <mailleux.tom@gmail.com>
2024-06-03 21:18:55 +0200

    Add PhoneNumberInput, update form styles, and enhance validation handling

--- app/contact/ContactForm.tsx
+++ app/contact/ContactForm.tsx
@@ -1,82 +1,91 @@
 'use client'
 import React, { useState } from 'react';
 import { useForm, ValidationError } from '@formspree/react';
-import { Box, Container, Grid, Input, TextField, Typography } from '@mui/material';
+import { Box, Button, Container, Grid, Typography, FormControl, InputLabel, OutlinedInput, FormHelperText, InputAdornment, ThemeProvider } from '@mui/material';
 import PhoneNumberInput from './PhoneNumberInput';
-
-export default function ContactForm() {
-    const [state, handleSubmit] = useForm("mdoqvjbe");
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-   
-    if (state.succeeded) {
-        return <p>Thanks for joining!</p>;
-    }
-    
-    return (
-      <Container component="main">
-        <Box
+export const ContactForm = () => {
+  const [state, handleSubmit] = useForm("mdoqvjbe");
+  const [firstName, setFirstName] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;  // A CHANGER
+  }
+  return (
+    <Container component="main">
+      <Box
         sx={{
           marginTop: 8,
+          marginBottom: 8,
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
         }}
-        >
+      >
         <Typography variant="h5" component="h2">Prendre rendez-vous</Typography>
-        <Box component="form" noValidate sx={{ mt:3, mb:3 }}>
-          <Grid container spacing={2}>
+        <Box component="form" noValidate sx={{ mt: 3, mb: 3 }} onSubmit={handleSubmit}>
+          <Grid container spacing={2} justifyContent={'center'}>
             <Grid item xs={12} sm={6}>
-              <TextField
-              InputLabelProps={{
-                sx:{
-                  fontSize: '1.5rem'
-                }
-              }}
-                name='firstName'
-                value={name}
-                onChange={((e)=> setName(e.target.value))}
-                required
-                fullWidth
-                id='firstName'
-                label='First Name'
-                autoFocus
+              <FormControl fullWidth variant="outlined" required>
+                <InputLabel htmlFor="firstName">Prénom</InputLabel>
+                <OutlinedInput
+                  id="firstName"
+                  name="firstName"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  label="Prénom"
                 />
+                <FormHelperText>{/* Error message or helper text if needed */}</FormHelperText>
+              </FormControl>
+              <ValidationError prefix='Prenom' field='firstname' errors={state.errors}/>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField
-              required
-              fullWidth
-              id='lastName'
-              label='Last Name'
-              name='LastName'
-              autoComplete='family-name'
-              />
+              <FormControl fullWidth variant="outlined" required>
+                <InputLabel htmlFor="lastName">Nom</InputLabel>
+                <OutlinedInput
+                  id="lastName"
+                  name="LastName"
+                  label="Nom"
+                  value={name}
+                  onChange={(e)=> setName(e.target.value)}
+                />
+                <FormHelperText>{/* Error message or helper text if needed */}</FormHelperText>
+              </FormControl>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField
-              required
-              fullWidth
-              id='emailAddress'
-              label='Email Address'
-              name='EmailAddress'
-              value={email}
-              onChange={((e)=> setEmail(e.target.value))}
-              autoComplete='EMAIL'
-              />
+              <FormControl fullWidth variant="outlined" required>
+                <InputLabel htmlFor="emailAddress">Adresse Email</InputLabel>
+                <OutlinedInput
+                  id="emailAddress"
+                  name="EmailAddress"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  label="Adresse Email"
+                />
+                <FormHelperText>{/* Error message or helper text if needed */}</FormHelperText>
+              </FormControl>
             </Grid>
             <Grid item xs={12} sm={6}>
-                <PhoneNumberInput/>
+              <PhoneNumberInput />
             </Grid>
             <Grid item xs={12} sm={6}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="message">Message</InputLabel>
+                <OutlinedInput multiline
+                  id="message"
+                  label="Message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+              </FormControl>
             </Grid>
-            
+            <Grid item xs={12} sm={12}>
+              <Button variant='contained' type='submit' disabled={state.submitting}>Reserver</Button>
+            </Grid>
           </Grid>
         </Box>
-
-        </Box>
-
-      </Container>
-    );
-  }
- 
+      </Box>
+    </Container>
+  );
+};
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,28 +3,34 @@ import Grid from "@mui/material/Grid";
 import { HeroSection } from "@/src/features/layout/HeroSection";
 import { Images } from "../assets/Images";
 import { useForm, ValidationError } from '@formspree/react';
-import ContactForm from "./ContactForm";
-import { Box } from "@mui/material";
-
-interface iframe{
+import { Box, ThemeProvider } from "@mui/material";
+import { ContactForm } from "./ContactForm";
+import { ActionButton } from "@/src/theme/ActionButton";
+interface iframe {
     allowfullscreen: string
 }
-const Contact = ({allowfullscreen}:iframe) => {
-    return(
-        <Grid>
-            <HeroSection titreh1='Rejoignez-moi pour une consultation à Vénissieux' imgSrc={Images.Banner[3]}/>
-            <Grid container>
-                <Grid  item xs={12} sm={6}>
-                    <ContactForm/>
-                </Grid>
-                <Grid  item xs={12} sm={6}>
-                <Box gap={5} p={2} sx={{borderRadius:'50%'}}>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2786.765754411849!2d4.865520576616394!3d45.69567271796406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4c27a70c6f3bf%3A0xa071b49323c80288!2s13%20Av.%20de%20la%20Division%20Leclerc%2C%2069200%20V%C3%A9nissieux%2C%20France!5e0!3m2!1sfr!2sus!4v1717075851777!5m2!1sfr!2sus" width="600" height="450"   loading="lazy" ></iframe>
-                </Box>
+const Contact = ({ allowfullscreen }: iframe) => {
+    return (
+        <ThemeProvider theme={ActionButton}>
+
+            <Grid>
+                <HeroSection titreh1='Rejoignez-moi pour une consultation à Vénissieux' imgSrc={Images.Banner[3]} />
+                <Grid container>
+                    <Grid item xs={12} sm={6}>
+                        <ContactForm />
+                    </Grid>
+                    <Grid item xs={12} sm={6} padding={1}>
+                        <Box className='map-container' sx={{
+                        }}>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2786.765754411849!2d4.865520576616394!3d45.69567271796406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4c27a70c6f3bf%3A0xa071b49323c80288!2s13%20Av.%20de%20la%20Division%20Leclerc%2C%2069200%20V%C3%A9nissieux%2C%20France!5e0!3m2!1sfr!2sus!4v1717075851777!5m2!1sfr!2sus"
+                                loading="lazy"
+                                title="Google Map"
+                            />
+                        </Box>
+                    </Grid>
                 </Grid>
             </Grid>
-        </Grid>
+        </ThemeProvider>
     )
-
 };
 export default Contact;
