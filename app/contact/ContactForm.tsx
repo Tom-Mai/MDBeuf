e56e63df22a002b9ec1ca98753ba9450d5134a03
Tom-Mai <mailleux.tom@gmail.com>
2024-06-04 09:54:42 +0200

    Validate form in progress; change formcontrol into Textfield

--- app/contact/ContactForm.tsx
+++ app/contact/ContactForm.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState } from 'react';
 import { useForm, ValidationError } from '@formspree/react';
-import { Box, Button, Container, Grid, Typography, FormControl, InputLabel, OutlinedInput, FormHelperText, InputAdornment, ThemeProvider } from '@mui/material';
+import { Box, Button, Container, Grid, Typography, FormControl, InputLabel, OutlinedInput, FormHelperText, Modal, TextField } from '@mui/material';
 import PhoneNumberInput from './PhoneNumberInput';
 export const ContactForm = () => {
   const [state, handleSubmit] = useForm("mdoqvjbe");
@@ -9,8 +9,32 @@ export const ContactForm = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   if (state.succeeded) {
-    return <p>Thanks for joining!</p>;  // A CHANGER
+    return (
+
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <Box>
+          <Typography id="modal-title" variant="h6" component="h2">
+            Message Envoyé
+          </Typography>
+          <Typography id="modal-description" sx={{ mt: 2 }}>
+            Merci pour votre demande, je reviendrai vers vous au plus vite pour vous confirmer celle-ci
+          </Typography>
+        </Box>
+
+      </Modal>
+
+
+    );  // A CHANGER
   }
   return (
     <Container component="main">
@@ -27,7 +51,18 @@ export const ContactForm = () => {
         <Box component="form" noValidate sx={{ mt: 3, mb: 3 }} onSubmit={handleSubmit}>
           <Grid container spacing={2} justifyContent={'center'}>
             <Grid item xs={12} sm={6}>
-              <FormControl fullWidth variant="outlined" required>
+              <TextField
+                required
+                type='text'
+                fullWidth
+                id='firstName'
+                name="firstName"
+                label="Prénom"
+                autoComplete='firstName'
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+              {/* <FormControl fullWidth variant="outlined" required>
                 <InputLabel htmlFor="firstName">Prénom</InputLabel>
                 <OutlinedInput
                   id="firstName"
@@ -36,12 +71,23 @@ export const ContactForm = () => {
                   onChange={(e) => setFirstName(e.target.value)}
                   label="Prénom"
                 />
-                <FormHelperText>{/* Error message or helper text if needed */}</FormHelperText>
-              </FormControl>
-              <ValidationError prefix='Prenom' field='firstname' errors={state.errors}/>
+              </FormControl> */}
+              <ValidationError prefix='Prenom' field='firstname' errors={state.errors} />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <FormControl fullWidth variant="outlined" required>
+              <TextField
+                required
+                type='text'
+                fullWidth
+                id='lastName'
+                label='Nom'
+                name='LastName'
+                autoComplete='family-name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+
+              />
+              {/* <FormControl fullWidth variant="outlined" required>
                 <InputLabel htmlFor="lastName">Nom</InputLabel>
                 <OutlinedInput
                   id="lastName"
@@ -50,27 +96,46 @@ export const ContactForm = () => {
                   value={name}
                   onChange={(e)=> setName(e.target.value)}
                 />
-                <FormHelperText>{/* Error message or helper text if needed */}</FormHelperText>
-              </FormControl>
+              </FormControl> */}
             </Grid>
             <Grid item xs={12} sm={6}>
-              <FormControl fullWidth variant="outlined" required>
+              <TextField
+                required
+                type='email'
+                fullWidth
+                id='emailAddress'
+                label='Adresse Email'
+                name='EmailAddress'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                autoComplete='EMAIL'
+              />
+              {/* <FormControl fullWidth variant="outlined" required>
                 <InputLabel htmlFor="emailAddress">Adresse Email</InputLabel>
-                <OutlinedInput
+                <OutlinedInput 
+                  type='email'
                   id="emailAddress"
                   name="EmailAddress"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   label="Adresse Email"
                 />
-                <FormHelperText>{/* Error message or helper text if needed */}</FormHelperText>
-              </FormControl>
+              </FormControl> */}
             </Grid>
             <Grid item xs={12} sm={6}>
               <PhoneNumberInput />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <FormControl fullWidth variant="outlined">
+              <TextField
+              fullWidth
+              multiline
+              id="message"
+              label="Message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              
+              />
+              {/* <FormControl fullWidth variant="outlined">
                 <InputLabel htmlFor="message">Message</InputLabel>
                 <OutlinedInput multiline
                   id="message"
@@ -78,10 +143,11 @@ export const ContactForm = () => {
                   value={message}
                   onChange={(e) => setMessage(e.target.value)}
                 />
-              </FormControl>
+              </FormControl> */}
             </Grid>
             <Grid item xs={12} sm={12}>
-              <Button variant='contained' type='submit' disabled={state.submitting}>Reserver</Button>
+              <Button variant='contained' type='submit' onClick={handleOpen} disabled={state.submitting}>Reserver</Button>
+
             </Grid>
           </Grid>
         </Box>
